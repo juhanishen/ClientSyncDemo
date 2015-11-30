d0c3398598456ed4dc327ac27b363a5ac4e5a782
juhanishen <juhanishen@gmail.com>
2015-11-30 23:48:11 +0200

    Initial skeleton
    solve conflict

---
+++ src/main/java/com/juhani/client/sync/demo/mongo/MongoConstants.java
@@ -0,0 +1,10 @@
+package com.juhani.client.sync.demo.mongo;
+
+public class MongoConstants {
+  public static final String SyncTokenCollectionName = "syncTokenCollection";
+  public static final String SyncValueCollectionName = "syncValueCollection";
+  public static final String SyncId = "syncId";
+  public static final String SyncToken = "token";
+  public static final String SyncValue = "value";
+  public static final String SyncDBName = "syncDB";
+}

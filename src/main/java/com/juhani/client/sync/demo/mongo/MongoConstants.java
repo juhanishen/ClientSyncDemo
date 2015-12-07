8e2094265d6058f8dd6b9948e85bb325785240cd
juhanishen <juhanishen@gmail.com>
2015-12-07 19:44:50 +0200

    format changes

--- src/main/java/com/juhani/client/sync/demo/mongo/MongoConstants.java
+++ src/main/java/com/juhani/client/sync/demo/mongo/MongoConstants.java
@@ -3,13 +3,9 @@ package com.juhani.client.sync.demo.mongo;
 public class MongoConstants {
   public static final String SyncTokenCollectionName = "syncTokenCollection";
   public static final String SyncValueCollectionName = "syncValueCollection";
-//  public static final String SyncEditModeCollectionName = "syncEditMode";
   public static final String SyncId = "syncId";
   public static final String SyncToken = "token";
   public static final String SyncValue = "value";
   public static final String SyncDBName = "syncDB";
   public static final String ClientName = "clientName";
-//  public static final String SyncMode = "mode";
-//  public static final String ModeEditing = "editingMode"; 
-//  public static final String ModeFree = "editingFree";
 }
